af170d733e28e16c10f5792e6394bd7dd99729db
Scott Gudeman <24697493+DragonCrafted87@users.noreply.github.com>
2026-09-05 08:38:27 -0500

    minor commentary clean up

--- config/hypr/scripts/spin-border.sh
+++ config/hypr/scripts/spin-border.sh
@@ -6,7 +6,6 @@
 # Seconds per full rotation. Match the look you wanted (~8s).
 SECONDS_PER_TURN="${SECONDS_PER_TURN:-6}"
 
-#GRADIENT="rgb(305cde) rgb(560591) rgb(780606) rgb(305cde)"
 GRADIENT="rgb(305cde) rgb(560591)"
 
 # ~45 updates/sec is enough at 2px border; cheaper than a 144 Hz compositor loop.
