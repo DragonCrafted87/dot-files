efc20b9f1234565b902e1091f57167769d7b5f72
Scott Gudeman <24697493+DragonCrafted87@users.noreply.github.com>
2026-09-05 09:56:26 -0500

    remove extraneous archive files

--- setup/archive/setup-git-bash.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# shellcheck disable=SC2187,SC2016,SC2094,SC2164,SC2317
-
-return
-
-wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-windows-amd64.exe -O ~/bin/oh-my-posh
-chmod +x ~/bin/oh-my-posh
