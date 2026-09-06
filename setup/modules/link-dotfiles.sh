256f0ff711cfa5b098536ba47edd60dbea0aeaf8
Scott Gudeman <24697493+DragonCrafted87@users.noreply.github.com>
2026-09-05 22:44:35 -0500

    Link ~/.bashrc to shell/linux.bashrc

--- setup/modules/link-dotfiles.sh
+++ setup/modules/link-dotfiles.sh
@@ -10,6 +10,9 @@ require_user
 ensure_repo "${DOTFILES_REPO_URL}" "${DOTFILES_DIR}"
 
 bashrc_source="${DOTFILES_DIR}/${DOTFILES_BASHRC}"
+if [[ ! -f "$bashrc_source" ]]; then
+    bashrc_source="${DOTFILES_DIR}/shell/linux.bashrc"
+fi
 [[ -f "$bashrc_source" ]] || die "missing ${bashrc_source}"
 [[ -d "${DOTFILES_DIR}/bashrc.d" ]] || die "missing ${DOTFILES_DIR}/bashrc.d"
 
