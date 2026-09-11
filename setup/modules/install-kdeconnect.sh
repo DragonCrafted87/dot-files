ea4f8c7db1ad68e931f7e6f89d256cf4837728ce
Scott Gudeman <24697493+DragonCrafted87@users.noreply.github.com>
2026-09-11 16:51:17 -0500

    Do not install KF5 breeze-icons; it conflicts with kf6-breeze-icons.

--- setup/modules/install-kdeconnect.sh
+++ setup/modules/install-kdeconnect.sh
@@ -11,6 +11,7 @@ require_user
 # Rock kdeconnect-sms is still Qt5 / Kirigami.2. SMS QML needs
 # qt5-qtmultimedia. Names need kpeoplevcard. Dark chrome needs Breeze
 # plus qqc2-desktop-style reading ~/.config/kdeglobals.
+# Do not install breeze-icons: it file-conflicts with kf6-breeze-icons.
 ensure_packages \
     kdeconnect \
     android-tools \
@@ -18,7 +19,6 @@ ensure_packages \
     kpeoplevcard \
     kpeople \
     breeze \
-    breeze-icons \
     qqc2-desktop-style \
     kf6-qqc2-desktop-style \
     lib64Qt6Multimedia \
