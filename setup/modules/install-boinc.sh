af70feea2cbf11160b19119caad6a9ddb7c43c9e
Scott Gudeman <24697493+DragonCrafted87@users.noreply.github.com>
2026-09-20 14:22:35 -0500

    install-boinc wrapper calls require_user

--- setup/modules/install-boinc.sh
+++ setup/modules/install-boinc.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 SETUP_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 # shellcheck disable=SC1091
 . "${SETUP_ROOT}/lib.sh"
+require_user
 # shellcheck disable=SC1091
 . "${SETUP_ROOT}/files/boinc/manager-setup.sh"
 # shellcheck disable=SC1091
